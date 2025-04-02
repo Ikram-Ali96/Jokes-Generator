895ddbad8fcfbaf2b959ce80b99846edfbe772d4
Ikram-Ali96 <ikram.abdullahi25@gmail.com>
2025-04-02 12:46:12 +0100

    Added basic JS and type writer plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-alert("hello World");
+function genarateJoke(event) {
+    event.preventDefault();
+    let jokeElement = ducoment.querySelector("#joke");
+
+    new Typewriter("#joke", {
+      strings: "What do you call an alligator in a vest? An investigator!",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+    
+}
+
+let jokeFormElement = document.querySelector("#joke-generator-form");
+jokeFormElement.addEventListener("submit" , genarateJoke);
