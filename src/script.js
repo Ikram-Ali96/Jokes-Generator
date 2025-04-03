b787a99e997e6717a759401797c4a095aa982786
Ikram-Ali96 <ikram.abdullahi25@gmail.com>
2025-04-03 14:41:11 +0100

    added more JS and HTML to my project

--- src/script.js
+++ src/script.js
@@ -17,10 +17,14 @@ function genarateJoke(event) {
     let context =
     "You are a funny AI comedian that generates British jokes. You're mission is to generate 2-3 lines jokes in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions below. Do not include a title to the joke. Sign the joke with 'SheCodes AI' inside a <strong> element at the end of the joke and NOT at the beginning.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    let jokeElement = document.querySelector("#joke"); 
+    jokeElement.classList.remove("hidden");
+    jokeElement.innerHTML = `Generating British joke about ${instructionsInput.value}`;
+
+
 
     axios.get(apiUrl).then(displayJoke);
-
-    
     
 }
 
