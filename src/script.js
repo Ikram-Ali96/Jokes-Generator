11ed8ec2b504fc01941b1721642cae6359cc1bbb
Ikram-Ali96 <ikram.abdullahi25@gmail.com>
2025-04-03 14:20:37 +0100

    Added AI API to project

--- src/script.js
+++ src/script.js
@@ -10,9 +10,12 @@ new Typewriter("#joke", {
 
 function genarateJoke(event) {
     event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "d6973oc76fcf3c7at603c4b3a18b2464";
-    let prompt = "";
-    let context = "";
+    let prompt = `User instructions: Generate funny British jokes ${instructionsInput.value}`;
+    let context =
+    "You are a funny AI comedian that generates British jokes. You're mission is to generate 2-3 lines jokes in basic HTML and seperate each line with a <br/>. Make sure to follow the user instructions below. Do not include a title to the joke. Sign the joke with 'SheCodes AI' inside a <strong> element at the end of the joke and NOT at the beginning.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(displayJoke);
